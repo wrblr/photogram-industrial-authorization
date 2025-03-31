aea5ad58ab9daefb5f6875a5dde13aa5b41ee077
Ben Purinton <ben.purinton@gmail.com>
2025-03-31 11:35:44 -0700

    Fix erd and spec annotations

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id             :bigint           not null, primary key
+#  caption        :text
+#  comments_count :integer          default(0)
+#  image          :string
+#  likes_count    :integer          default(0)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  owner_id       :bigint           not null
+#
+# Indexes
+#
+#  index_photos_on_owner_id  (owner_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (owner_id => users.id)
+#
 require "rails_helper"
 
 RSpec.describe Photo, type: :model do
